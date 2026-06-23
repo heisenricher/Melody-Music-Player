60c7442cea578ebc3d8f59c2d887665e2c913c29
heisenricher <heisenricher@gmail.com>
2026-06-23 08:28:20 +0530

    Bump versionCode to 5 and versionName to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.melody"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
